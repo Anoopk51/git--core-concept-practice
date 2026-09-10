e754f2d9f7ce1f2c9eef77365bd9de438ff7deec
Anoop Kushwaha <anoopit41@gmail.com>
2026-09-10 13:48:43 +0530

    feat: add new file oopPractice.py

---
+++ oopPractice1.py
@@ -0,0 +1,24 @@
+'''
+1.
+'''
+
+class Institute:
+
+    def __init__(self,name,stablishYear):
+        self.name = name
+        self.stablishYear = stablishYear
+
+class Student(Institute):
+
+    def __init__(self,stuName,age,branch,name,stablishYear):
+        super().__init__(name,stablishYear)
+        self.stuName = stuName
+        self.age =  age
+        self.branch = branch
+
+obj = Student("Anoop",22,"IT","SCRIET",2003)
+print(obj.stuName)
+print(obj.age)
+print(obj.branch)
+print(obj.name)
+print(obj.stablishYear)
--- sumoOfTwoNum.py
+++ sumoOfTwoNum.py
@@ -1,11 +1,25 @@
-<<<<<<< HEAD
-=======
 
-
->>>>>>> main
 def func(num1,num2):
     return num1+num2
 num1 = 4
 num2 = 3    
 a=func(num1,num2)
 print(a)
+
+
+# def product(l):
+#     for i in l:
+#         a =list(lambda  i: i**2)
+
+l = [1,2,3,4,5]
+# for i in l:
+a=list(map(lambda i: i ** 2,l))
+print(a)
+
+print(l)
+# print(product(l))
+
+
+
+# x = lambda a,b : a+b
+# print(x(3,4))
